5575ab50d0769bba51c350b01af7daf7abe75c7f
Nimish Bongale <nimishnb98@gmail.com>
2019-02-11 23:07:52 +0530

    Add files via upload

--- FourierTransformOfImage/FourierTransform.py
+++ FourierTransformOfImage/FourierTransform.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+img = cv2.imread('graf[1].jpg',0)
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift)) ###the magnitude may thus be accordingly altered
+
+plt.subplot(121)
+plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122)
+plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Fourier Transform of the Image'), plt.xticks([]), plt.yticks([])
+plt.show()
