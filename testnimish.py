0b31b253bc5f61c84b5d2486ead550b5c4b19de6
Nimish Bongale <nimishnb98@gmail.com>
2019-08-25 15:19:17 +0530

    Add files via upload

---
+++ testnimish.py
@@ -0,0 +1,1 @@
+for i in range(int(input())):u=input;f=lambda x:[x.extend(list(map(int,u())))for i in m];s=lambda x:sum(x);m=range(int(u().split()[0]));f(a);f(b);print((s(a)-s([x*y for x,y in zip(a,b)]),-1)[s(a)!=s(b)])
